9f5467c01e9cba0d07e8bcc117b305c15e9bbe88
Jordan Smalls <smalls.jordan@gmail.com>
2023-08-06 16:24:59 -0400

    added challenge

---
+++ 8kyu/multiply.js
@@ -0,0 +1,14 @@
+// Multiply
+// This code does not execute properly. Try to figure out why.
+
+// given code
+function multiply(a, b){
+    a * b
+  }
+  
+
+function multiply(a,b){
+    let product;
+    product = (a * b)
+    return product
+}
