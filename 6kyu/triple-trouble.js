ae92a87b651c7bb771cdcd11f0e03a2b89a9021e
Jordan Smalls <smalls.jordan@gmail.com>
2023-08-11 16:36:59 -0400

    added 6kyu folder with challenge

---
+++ 6kyu/triple-trouble.js
@@ -0,0 +1,18 @@
+// Triple trouble
+
+// Write a function - tripledouble(num1,num2)
+
+// which takes numbers num1 and num2 and returns 1 if there is a straight triple of a number at any place in num1 and also a straight double of the same number in num2.
+
+// If this isn't the case, return 0
+
+function tripledouble(num1, num2) {
+  for (let i = 0; i < 10; i++) {
+    if (new RegExp(`${i}{3}`).test(num1) && new RegExp(`${i}{2}`).test(num2)) {
+      return 1;
+    }
+  }
+  return 0;
+}
+
+// tests passes
