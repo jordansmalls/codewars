4853766936547585baaec63aa1587bf28c70c806
Jordan Smalls <smalls.jordan@gmail.com>
2023-08-15 19:51:48 -0400

    added challenge

---
+++ 8kyu/square-sum-array.js
@@ -0,0 +1,26 @@
+// Square(n) Sum
+//TODO Complete the square sum function so that it squares each number passed into it and then sums the results together.
+// For example, for [1, 2, 2] it should return 9 
+
+function squareSum(numbers){
+    let result = 0;
+    for (let i = 0; i < numbers.length; i++){
+        result += (numbers[i] * numbers[i])
+    }
+    return result
+};
+
+
+// refactored solution --> preferred
+function squareSum2(numbers){
+    return numbers.reduce((sum,num) => sum + (num * num), 0);
+};
+
+
+
+testNums = [1,2,2];
+
+let test1 = squareSum(testNums);
+let test2 = squareSum2(testNums);
+
+// both test variables are set to 9 --> tests pass
