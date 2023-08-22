37584f7b1e74fd44024b2e4d2c881142aca7f6a1
Jordan Smalls <smalls.jordan@gmail.com>
2023-08-21 21:53:26 -0400

    added challenge

---
+++ 8kyu/convert-number-to-a-string.js
@@ -0,0 +1,11 @@
+// Convert a Number to a String!
+
+// We need a function that can transform a number (integer) into a string.
+
+// Example
+// 123  --> "123"
+
+
+const  numberToString = (num) => {return num.toString()}
+
+// test cases pass
