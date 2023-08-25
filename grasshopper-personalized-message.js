e0b8d92e6b8f33bdd15b6befd8956a9ece284ff3
Jordan Smalls <smalls.jordan@gmail.com>
2023-08-25 15:04:58 -0400

    added challenge

---
+++ grasshopper-personalized-message.js
@@ -0,0 +1,20 @@
+// Grasshopper - Personalized Message
+
+// Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+// Use conditionals to return the proper message:
+
+// case	                return
+
+// name equals owner	'Hello boss'
+// otherwise	        'Hello guest'
+
+const greet = (name,owner) => {
+    if(name === owner){
+        return 'Hello boss'
+    }else{
+        return 'Hello guest'
+    }
+};
+
+// test cases pass
