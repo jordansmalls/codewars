d8894a08d4ff3a3f1fb1724a77e76cee7e92127b
Jordan Smalls <smalls.jordan@gmail.com>
2023-08-25 14:57:54 -0400

    added challenge

---
+++ convert-boolean-to-a-string.js
@@ -0,0 +1,14 @@
+// Convert a Boolean to a String
+
+// Implement a function which convert the given boolean value into its string representation.
+
+
+const booleanToString = (b) => {
+    if(b === true){
+        return 'true'
+    }else{
+        return 'false'
+    }
+}
+
+// test cases pass
