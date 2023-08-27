1a9b4d87e730245def41e6eeba0450a23328101d
Jordan Smalls <smalls.jordan@gmail.com>
2023-08-27 19:28:04 -0400

    added challenge

---
+++ calculate-average.js
@@ -0,0 +1,15 @@
+// Calculate average
+
+// Write a function which calculates the average of the numbers in a given list.
+
+// Note: Empty arrays should return 0.
+
+const findAverage = arr => {
+    let sum = 0;
+    for(let i = 0; i < arr.length; i++){
+        sum += arr[i]
+    }
+    let ans;
+    ans = (sum / arr.length)
+    return ans
+};
