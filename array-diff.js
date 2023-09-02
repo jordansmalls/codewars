23ce898645b32c682ba89c25fb764fb6b297d6c3
Jordan Smalls <smalls.jordan@gmail.com>
2023-09-02 15:30:34 -0400

    subtracts an array from another array

---
+++ array-diff.js
@@ -0,0 +1,10 @@
+// Array.diff
+
+// Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+
+// It should remove all values from list a, which are present in list b keeping their order.
+
+// arrayDiff([1,2],[1]) == [2]
+
+const arrayDiff =(a,b)=>a.filter(e => !b.includes(e));
+  
