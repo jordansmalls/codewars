fbe3dfff345405c005452ef3d472c76683226a9b
Jordan Smalls <smalls.jordan@gmail.com>
2023-09-06 17:39:59 -0400

    converts input of hours into seconds

---
+++ convert-hours-into-seconds.js
@@ -0,0 +1,9 @@
+// Convert Hours into Seconds
+
+// Write a function that converts hours into seconds.
+// EASY REVIEW
+
+const hoursToSeconds = hours => ((hours * 60) * 60);
+
+console.log(hoursToSeconds(24)) // 86400
+// test passes
