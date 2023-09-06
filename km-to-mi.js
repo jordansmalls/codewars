f7e4cb38a5500d96b860509537ad8e3a2e6cf4f6
Jordan Smalls <smalls.jordan@gmail.com>
2023-09-06 16:46:26 -0400

    converts kilometers to miles

---
+++ km-to-mi.js
@@ -0,0 +1,12 @@
+// Km to Mi Conversion
+
+// Creating a function is a skill that’ll be useful in many settings, and as you progress, you’ll be working on much more complicated functions than this one. But a function that converts units of measure can be pretty handy in multiple professions and industries.
+
+// Let’s start with a conversion from kilometers to miles. The function should include the input in kilometers and return the answer in miles. Round to the nearest whole integer.
+
+
+const kilometersToMiles = km => Math.round(km * 0.621371)
+
+kilometersToMiles(30) // 19
+
+// test pass
