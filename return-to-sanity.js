aa5d3afa63c05157da0f23397564a38055667bc6
Jordan Smalls <smalls.jordan@gmail.com>
2023-09-06 13:49:31 -0400

    returns an object

---
+++ return-to-sanity.js
@@ -0,0 +1,10 @@
+// Return to Sanity
+
+// This function should return an object, but it's not doing what's intended. What's wrong?
+
+const mystery = _ => {
+    let results = {
+        sanity: 'Hello'
+    }
+    return results
+};
