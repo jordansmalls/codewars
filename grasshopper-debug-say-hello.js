30a4c3ee3e84555250a6e366ee6ccac3de5c1dce
Jordan Smalls <smalls.jordan@gmail.com>
2023-09-16 14:27:50 -0400

    returns a string including the functions input

---
+++ grasshopper-debug-say-hello.js
@@ -0,0 +1,16 @@
+// Grasshopper - Debug sayHello
+
+// The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+// Example output:
+// Hello, Mr. Spock
+
+
+// what you have to debug:
+
+function sayHello(name) {
+    return 'Hello'
+  }
+
+//
+
+const sayHello = name => `Hello, ${name}`
