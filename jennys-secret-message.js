0f8dfa5987305a7270f252bd565a751780190fef
Jordan Smalls <smalls.jordan@gmail.com>
2023-09-18 17:38:41 -0400

    debug for greet function

---
+++ jennys-secret-message.js
@@ -0,0 +1,16 @@
+// Jenny's secret message
+
+
+// Jenny has written a function that returns a greeting for a user. However, she's in love with Johnny, and would like to greet him slightly different. She added a special case to her function, but she made a mistake.
+
+// Jenny's code:
+
+function greeet(name){
+    return "Hello, " + name + "!";
+    if(name === "Johnny")
+      return "Hello, my love!";
+  }
+
+// solution 
+
+const greet = name => name === 'Johnny' ? 'Hello, my love!' : `Hello, ${name}!`;
