346eda85ce1ed1f16cc7eede706a41f7343130f3
Jordan Smalls <smalls.jordan@gmail.com>
2023-09-19 16:59:24 -0400

    determines if an integer argument is even or odd

---
+++ even-or-odd-.py
@@ -0,0 +1,6 @@
+# Even or Odd
+
+# Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+def even_or_odd(n):
+    return 'Even' if n % 2 == 0 else 'Odd'
