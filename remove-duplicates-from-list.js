49d2134bb14b6456bc9ea8b7d9901b974b8d9856
Jordan Smalls <smalls.jordan@gmail.com>
2023-09-19 13:12:24 -0400

    removes duplicates from an array of non negative numbers and returns it as a result

---
+++ remove-duplicates-from-list.js
@@ -0,0 +1,13 @@
+/* 
+
+Remove duplicates from list
+
+
+Define a function that removes duplicates from an array of non negative numbers and returns it as a result.
+
+The order of the sequence has to stay the same.
+
+*/
+
+
+const distinct = a => Array.from(new Set(a))
