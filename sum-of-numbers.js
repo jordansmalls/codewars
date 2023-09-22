de7ad49575d8b21be1cab27164b92bffe4e6e1ce
Jordan Smalls <smalls.jordan@gmail.com>
2023-09-22 16:29:19 -0400

    finds the sum of all the integers between and including two arguments and returns it

---
+++ sum-of-numbers.js
@@ -0,0 +1,34 @@
+// Sum of Numbers
+
+// Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+
+// Examples (a, b) --> output (explanation)
+// (1, 0) --> 1 (1 + 0 = 1)
+// (1, 2) --> 3 (1 + 2 = 3)
+// (0, 1) --> 1 (0 + 1 = 1)
+// (1, 1) --> 1 (1 since both are same)
+// (-1, 0) --> -1 (-1 + 0 = -1)
+// (-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+
+
+const getSum = (a,b) => {
+    let counter = [];
+    if(a < b){
+      for(let i = a; i <= b; i++){
+        counter.push(i)
+      }
+    }else{
+      for(let i = b; i <= a; i++){
+        counter.push(i)
+      }
+    }
+    return counter.reduce((acc,c)=>acc+c,0)
+  }
+
+  // tests pass
+
+
+
+// alternate solution
+
+const getSumTwo = (a, b) => (a + b) * (Math.abs(a - b) + 1) / 2;
