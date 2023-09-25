afe47c1f1af307cae10a59f99cf0f1636e35cf71
Jordan Smalls <smalls.jordan@gmail.com>
2023-09-25 14:09:02 -0400

    finds the sum of three inputs

---
+++ grasshopper-messi-goals-function.js
@@ -0,0 +1,8 @@
+// Grasshopper - Messi goals function
+
+// Complete the function to return his total number of goals inall three leagues
+
+// Note: the input will always be valid
+
+//easy practice
+const goals = (x,y,z) => x + y + z
