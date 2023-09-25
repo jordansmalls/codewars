c0dce9b31dea1878eab2be22ff0c4fd878ca44c6
Jordan Smalls <smalls.jordan@gmail.com>
2023-09-25 14:13:06 -0400

    practice debug - function adds 5 to any input

---
+++ grasshopper-basic-function-fixer.js
@@ -0,0 +1,14 @@
+// Grasshopper - Basic Function Fixer
+
+// I created this function to add five to any number that was passed in to it and return the new value. It doesn't throw any errors but it returns the wrong number.
+
+// Can you help me fix the function?
+
+function addFive(num) {
+  var total = num + 5
+  // should return total not num.
+  return num
+}
+
+
+const addFive = n => n += 5
