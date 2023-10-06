5bc984fe170623a2a594f33ce275eeafb4856893
Jordan Smalls <smalls.jordan@gmail.com>
2023-10-06 13:34:34 -0400

    returns a string with all five or more letter words reversed

---
+++ stop-gninnips-my-sdrow.js
@@ -0,0 +1,24 @@
+// Stop spinning my words
+
+/*
+Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+
+Examples:
+
+spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" 
+spinWords( "This is a test") => returns "This is a test" 
+spinWords( "This is another test" )=> returns "This is rehtona test"
+*/
+
+const spinWords = str => {
+    let arr = str.split(' ')
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i].length >= 5){
+            arr[i] = arr[i].split('').reverse().join('')
+        }
+    }
+    return arr.join(' ')
+}
+
+// tests pass
+
