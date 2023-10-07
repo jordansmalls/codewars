772372a8fe90541e597b4ab3c02a4383081caeaa
Jordan Smalls <smalls.jordan@gmail.com>
2023-10-07 15:03:43 -0400

    returns what a person should drink based on an input of age

---
+++ drink-about.js
@@ -0,0 +1,33 @@
+// Drink About
+
+/*
+Make a function that receive age, and return what they drink.
+
+Rules:
+
+Children under 14 old.
+Teens under 18 old.
+Young under 21 old.
+Adults have 21 or more.
+Examples: (Input --> Output)
+
+13 --> "drink toddy"
+17 --> "drink coke"
+18 --> "drink beer"
+20 --> "drink beer"
+30 --> "drink whisky"
+*/
+
+const peopleWithAgeDrink = x => {
+    if(x < 14){
+        return 'drink toddy'
+    }else if(x >= 14 && x < 18){
+        return 'drink coke'
+    }else if(x >= 18 && x < 21 ){
+        return 'drink beer'
+    }else{
+        return 'drink whisky'
+    }
+}
+
+// tests pass
