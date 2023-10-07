97020e0f451b9345b69aac8630b00b101a650c97
Jordan Smalls <smalls.jordan@gmail.com>
2023-10-07 15:04:54 -0400

    determines what an individual should drink according to an input of age

--- drink-about.js
+++ drink-about.js
@@ -31,3 +31,10 @@ const peopleWithAgeDrink = x => {
 }
 
 // tests pass
+
+// diff syntax
+
+const peopleWithAgeDrinkTwo = (age) =>
+  age < 14 ? "drink toddy" :
+  age < 18 ? "drink coke" :
+  age < 21 ? "drink beer" : "drink whisky"
