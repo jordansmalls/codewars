725d908d6b6026bf94ef76d67b33a2554bab18ef
Jordan Smalls <smalls.jordan@gmail.com>
2023-10-09 11:48:25 -0400

    returns true if the first argument ends with the second argument

---
+++ string-ends-with.js
@@ -0,0 +1,12 @@
+// String ends with?
+/*
+Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+Examples:
+solution('abc', 'bc') // returns true
+solution('abc', 'd') // returns false
+*/
+
+const solution = (s1, s2) => s1.endsWith(s2)
+
+// tests pass
