fc06ae9552c997036734cfc199fae737d403d4c6
Jordan Smalls <smalls.jordan@gmail.com>
2023-10-11 11:24:45 -0400

    returns an array of the last even numbers of a given array, with a length of number

---
+++ even-numbers-in-an-array.js
@@ -0,0 +1,20 @@
+// Even Numbers in an Array
+
+/*
+Given an array of numbers, return a new array of length number containing the last even numbers from the original array (in the same order). The original array will be not empty and will contain at least "number" even numbers.
+*/
+
+const evenNumbers = (array, number) => {
+    let res = [];
+    for(let i = 0; i <= array.length; i++){
+        if(array[i] % 2 == 0){
+            res.push(array[i])
+        };
+        if(res.length > number){
+            res.shift()
+        }
+    }
+    return res
+}
+
+// tests pass
