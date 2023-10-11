23363a1455195f671f5aeb8e30bf9a0f97ef0d39
Jordan Smalls <smalls.jordan@gmail.com>
2023-10-11 16:00:07 -0400

    returns the factorial of a given integer input

--- factorial.js
+++ factorial.js
@@ -23,3 +23,7 @@ function factorial(n) {
       throw new RangeError();
     return n <= 1 ? 1 : n * factorial(n - 1);
   }
+
+
+// one line solution :)
+const factorial = n => n < 0 || n > 12 ? (() => { throw new RangeError(); })() : n < 2 ? 1 : n * factorial(n - 1);
