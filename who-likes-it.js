d4ebe1cc48d23713c2dcbc6d56e812c7f7725727
Jordan Smalls <smalls.jordan@gmail.com>
2023-10-12 13:24:08 -0400

    takes an array containing the names of people that like an item, and returns the information formatted

--- who-likes-it.js
+++ who-likes-it.js
@@ -33,3 +33,21 @@ let test = ["Alex", "Jacob", "Mark", "Max"]
 likes(test)
 
 // passes tests
+
+
+// diff syntax
+
+function likes(names) {
+    switch(names.length) {
+      case 0:
+        return 'no one likes this';
+      case 1:
+        return `${names[0]} likes this`;
+      case 2:
+        return `${names[0]} and ${names[1]} like this`;
+      case 3:
+        return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+      default:
+        return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+    }
+  }
