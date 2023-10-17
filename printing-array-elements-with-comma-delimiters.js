bd5e1ddca50ac3a7d9380c814af850d89625d728
Jordan Smalls <smalls.jordan@gmail.com>
2023-10-17 13:53:21 -0400

    returns a string with comma delimiter

---
+++ printing-array-elements-with-comma-delimiters.js
@@ -0,0 +1,10 @@
+// Printing Array Elements with Comma Delimiters
+/*
+Input: Array of elements
+["h","o","l","a"]
+Output: String with comma delimited elements of the array in th same order.
+"h,o,l,a"
+*/
+
+const printArray = array => array.join(',')
+//pass
