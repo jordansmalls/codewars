50d62a41d2c6c3b07c91a4498f02c65092546ed1
Jordan Smalls <smalls.jordan@gmail.com>
2023-10-31 12:34:40 -0400

    returns a negative num

---
+++ return-neg.js
@@ -0,0 +1,11 @@
+// Return Negative
+/*
+In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+*/
+const makeNegative = n => n < 0 ? n : n*-1
+
+console.log(makeNegative(55), -55)
+console.log(makeNegative(-82), -82)
+console.log(makeNegative(23), -23)
+
+//tests pass
