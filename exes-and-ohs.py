db441f1f4ee012af5f10fe7eea67a06a5485631d
Jordan Smalls <smalls.jordan@gmail.com>
2023-11-03 12:39:13 -0400

    returns a boolean value, based on a given str containing the same num of x's and o's

---
+++ exes-and-ohs.py
@@ -0,0 +1,17 @@
+# Exes and Ohs
+# Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+# Examples input/output:
+
+# XO("ooxx") => true
+# XO("xooxx") => false
+# XO("ooxXm") => true
+# XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+# XO("zzoo") => false
+
+def xo(s):
+    count_x = s.lower().count('x')
+    count_o = s.lower().count('o')
+    return count_x == count_o
+
+# tests pass
