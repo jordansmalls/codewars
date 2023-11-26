4a44f94f8c11ebca6e208d9a32fe952d4004e310
Jordan Smalls <smalls.jordan@gmail.com>
2023-11-26 14:43:50 -0500

    returns a new function that always returns Hello World

---
+++ create-hello-world-function.js
@@ -0,0 +1,17 @@
+// 2667. Create Hello World Function
+/*
+Write a function createHelloWorld. It should return a new function that always returns "Hello World".
+Example 1:
+Input: args = []
+Output: "Hello World"
+Explanation:
+const f = createHelloWorld();
+f(); // "Hello World"
+The function returned by createHelloWorld should always return "Hello World".
+*/
+
+var createHelloWorld = () => {
+    return function helloWorld(){
+        return 'Hello World'
+    }
+}
