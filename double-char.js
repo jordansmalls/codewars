3adf7e3500e204464e164c0b09d584f7b4d68b31
Jordan Smalls <smalls.jordan@gmail.com>
2023-11-30 22:24:29 -0500

    returns a string in which each character (case-sensitive) is repeated once

---
+++ double-char.js
@@ -0,0 +1,20 @@
+// Double Char
+/*
+Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+*/
+
+const doubleChar = str => {
+    let arr = str.split('')
+    let res = []
+    for(let i = 0; i < arr.length; i++){
+      res.push(arr[i])
+      res.push(arr[i])
+    }
+    return res.join('')
+  }
+
+  
+console.log(doubleChar('String'), "SSttrriinngg")
+console.log(doubleChar('Hello World'), "HHeelllloo  WWoorrlldd")
+console.log(doubleChar("1234!_ "), "11223344!!__  ")
+console.log(doubleChar('jordan'), 'jjoorrddaann')
