2d1a936eeecfac0717fc7c506eda15dc252e3c80
Jordan Smalls <smalls.jordan@gmail.com>
2023-12-03 17:51:30 -0500

    returns a greeting statement that inlcudes the input name

---
+++ return-string.js
@@ -0,0 +1,12 @@
+// Return String
+/*
+Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+*/
+
+const greeting = name => `Hello, ${name} how are you doing today?`
+
+console.log(greeting('Bryan'),'Hello, Bryan how are you doing today?')
+console.log(greeting('Laila'),'Hello, Laila how are you doing today?')
+console.log(greeting('Joe'),'Hello, Joe how are you doing today?')
+
+// tests pass
