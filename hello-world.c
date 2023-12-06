0b491b78f254ffaa3094ff7b557848a089f70a53
Jordan Smalls <smalls.jordan@gmail.com>
2023-12-06 13:46:55 -0500

    returns a string of "Hello, World!"

---
+++ hello-world.c
@@ -0,0 +1,13 @@
+// Hello, World!
+/*
+Write a C function that returns the string "Hello, World!".
+Your solution should focus on returning the string "Hello, World!" using C syntax. Avoid using any external libraries or functions beyond the standard C library.
+*/
+
+#include <stdio.h>
+
+int main(){
+    printf("Hello, World!\n");
+    return 0;
+}
+
