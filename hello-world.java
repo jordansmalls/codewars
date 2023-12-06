1dcd1b88f9450c33478d189034390ad817db8ba3
Jordan Smalls <smalls.jordan@gmail.com>
2023-12-06 14:02:21 -0500

    prints "Hello, World!" to the console.

---
+++ hello-world.java
@@ -0,0 +1,10 @@
+// Hello, World! using Java
+/*
+Print the iconic "Hello, World!" to the console, using Java.
+*/
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
