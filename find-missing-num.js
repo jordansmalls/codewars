d72aa7550763fa8e88ee491b42bc15354adcbe7d
Jordan Smalls <smalls.jordan@gmail.com>
2023-12-15 15:43:58 -0500

    given an array of nums that are incrementing at a constant rate, this function returns the missing num from the array

--- find-missing-num.js
+++ find-missing-num.js
@@ -22,6 +22,8 @@ console.log(findMissingNumber([3,0,1]), 2)
 console.log(findMissingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1]), 8)
 console.log(findMissingNumber([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,24]), 23)
 
+// pass
+// more concise solution
 function findMissingNumberAgain(arr){
     let sort = arr.sort((a,b)=>a-b);
     for(let i = 0; i < sort.length; i++){
@@ -32,5 +34,6 @@ function findMissingNumberAgain(arr){
     return sort.length;
 }
 
-console.log(findMissingNumber([3,0,1,2,4,6]), 5)
-console.log(findMissingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1]), 8)
+console.log(findMissingNumberAgain([3,0,1,2,4,6]), 5)
+console.log(findMissingNumberAgain([9, 6, 4, 2, 3, 5, 7, 0, 1]), 8)
+// test cases pass
