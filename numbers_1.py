dc0df234cfcfe4d0d1959c57d9defe0b4efdafa7
Jordan Smalls <smalls.jordan@gmail.com>
2023-12-22 19:01:00 -0500

    simple python exercise

---
+++ numbers_1.py
@@ -0,0 +1,10 @@
+# Numbers
+# You are going shopping. Let’s make a grocery list so that you can plan your budget. Store the number of cucumbers you want to buy in a variable called cucumbers. Make sure it’s at least 1, and that it’s the appropriate datatype! The store doesn’t sell partial cucumbers.
+
+# Each cucumber costs 3.25 doubloons. Store the price per cucumber in a variable called price_per_cucumber. Create a new variable called total_cost which is the product of how many cucumbers you are going to buy and the cost per cucumber. Print out total_cost.
+
+
+cucumbers = 5
+price_per_cucumber = 3.25
+total_cost = price_per_cucumber * cucumbers
+print(total_cost)
