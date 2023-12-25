ada396b9f6a2fd1fc0424ab95bce82c8d9b95ae2
Jordan Smalls <smalls.jordan@gmail.com>
2023-12-24 21:06:28 -0500

    removes all exclamation marks from a given string, but ensures that an exclamation mark will be at the end of the string.

---
+++ remove-all-exclamations.js
@@ -0,0 +1,20 @@
+// Exclamation marks series #4: Remove all exclamation marks from sentence but ensure a exclamation mark at the end of string
+/*
+Remove all exclamation marks from sentence but ensure a exclamation mark at the end of string. For a beginner kata, you can assume that the input data is always a non empty string, no need to verify it.
+
+Examples
+"Hi!"     ---> "Hi!"
+"Hi!!!"   ---> "Hi!"
+"!Hi"     ---> "Hi!"
+"!Hi!"    ---> "Hi!"
+"Hi! Hi!" ---> "Hi Hi!"
+"Hi"      ---> "Hi!"
+
+*/
+const remove = str => str.split('!').join('') + '!';
+
+//console.log(remove("!Hi!"),"Hi!")
+//console.log(remove("Hi"), "Hi!")
+//console.log(remove("Hi!!!"), "Hi!")
+
+// test cases pass
