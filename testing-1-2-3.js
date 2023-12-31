95c5b60743c00f56e3b96bad95acb0dcbef3b61a
Jordan Smalls <smalls.jordan@gmail.com>
2023-12-30 20:04:39 -0500

    the number function takes an array of strings, and returns each line prepended by the correct corresponding number. the format is "n: string"

---
+++ testing-1-2-3.js
@@ -0,0 +1,24 @@
+// Testing 1-2-3
+/*
+Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+Write a function which takes a list of strings and returns each line prepended by the correct number.
+The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+
+Examples: (Input --> Output)
+
+[] --> []
+["a", "b", "c"] --> ["1: a", "2: b", "3: c"]
+*/
+
+// const number = array => {
+//     return array.map((word,index) => {
+//         return `${index + 1}: ${word}`
+//     })
+// };
+
+const number = array => array.map((word, index)=> `${index + 1}: ${word}`);
+
+console.log(number([]), []);
+console.log(number(["a", "b", "c"] ), ["1: a", "2: b", "3: c"]);
+
+// tests pass
