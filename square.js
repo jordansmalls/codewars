ec6a23f5335d69ccd662f2b3e34e8937154e1bd8
Jordan Smalls <smalls.jordan@gmail.com>
2024-01-04 18:53:27 -0500

    these functions implement squaring an int

---
+++ square.js
@@ -0,0 +1,20 @@
+// Square Function
+/*
+Now you have to write a function that takes an argument and returns the square of it.
+*/
+
+const square = n => n**2;
+
+const squareAgain = n => Math.pow(n, 2);
+
+function squareAgainAgain(n){
+    return n * n
+}
+
+const squareAgainAgainAgain = num => {
+    if(typeof num === 'number')return num**2;
+    else return undefined
+};
+
+
+// all tests pass =)
