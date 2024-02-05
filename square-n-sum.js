bc7e46813d8101fbe93263ef4872f6250c01f393
Jordan <smalls.jordan@gmail.com>
2024-02-05 18:49:12 -0500

    added starting val for reduce method

--- square-n-sum.js
+++ square-n-sum.js
@@ -3,7 +3,7 @@
 Complete the square sum function so that it squares each number passed into it and then sums the results together.
 */
 
-const squareSum = arr => arr.reduce((a,c)=>a+c**2);
+const squareSum = arr => arr.reduce((a,c)=>a+c**2,0);
 
 const nums = [1,2,2] // 9
 console.log(squareSum(nums) === 9) // true
