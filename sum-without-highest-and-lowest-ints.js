d70af1acbec2275f08136f57702ee7c52dc501d6
jordansmalls <smalls.jordan@gmail.com>
2024-02-15 19:55:24 -0500

    catching up repo

--- sum-without-highest-and-lowest-ints.js
+++ sum-without-highest-and-lowest-ints.js
@@ -14,14 +14,12 @@ Input validation
 
 If an empty value ( null, None, Nothing etc. ) is given instead of an array, or the given array is an empty list or a list with only 1 element, return 0.
 */
-// const sumArray = a => {
-//     a = a.sort((a,b)=>a-b);
-//     a.pop();
-//     a.shift();
-//     return a.reduce((a,c)=>a+c,0)
-// }
-
-const sumArray = a => a.slice().sort((a, b) => a - b).slice(1, -1).reduce((a, c) => a + c, 0);
+const sumArray = a => {
+    a = a.sort((a,b)=>a-b);
+    a.pop();
+    a.shift();
+    return a.reduce((a,c)=>a+c,0)
+}
 
 const x = [ 6, 2, 1, 8, 10 ];
 const y = [ 1, 1, 11, 2, 3 ];
