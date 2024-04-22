0d822a7b09436e183dedb0857ed3c6e1ec878b08
Jordan Smalls <smalls.jordan@gmail.com>
2024-04-22 18:51:17 -0400

    receives an input string and returns the number of vowels present

---
+++ vowel-count-code.js
@@ -0,0 +1,27 @@
+// Count the Vowels
+/*
+Return the number (count) of vowels in the given string.
+We will consider a, e, i, o, u as vowels for this Kata (but not y).
+The input string will only consist of lower case letters and/or spaces.
+*/
+
+const getCount = str => {
+  var count = 0;
+  var temp = str.split('').forEach(char => {
+    if(char == 'a' || char == 'e' || char == 'i' || char == 'o' || char == 'u'){
+      count++;
+    }
+  })
+  return count
+}
+
+
+const getCountAlt = str => str.split('').filter(c => "aeiouAEIOU".includes(c)).length;
+ 
+
+console.log(getCount('jay')) // 1
+console.log(getCount('carson')) // 2
+console.log(getCount('bojack horseman')) // 5
+console.log(getCountAlt('bojack horseman')) // 5
+
+// tests pass
