e2564204a1d6b0ada144ca29e5edb55edbbc1967
Jordan <smalls.jordan@gmail.com>
2024-07-04 22:15:09 -0400

    receives 3 int inputs, returns the maximum value possible containing mathematical operators

---
+++ expressions-matter.js
@@ -0,0 +1,18 @@
+// Expressions Matter
+/*
+Given three integers a ,b ,c, return the largest number obtained after inserting the following operators and brackets: +, *, ()
+In other words , try every combination of a,b,c with [*+()] , and return the Maximum Obtained
+*/
+
+var expressionMatter = function(a, b, c) {
+  return Math.max(
+    a + b + c,
+    a * b * c,
+    a * (b + c),
+    (a + b) * c,
+    a + b * c,
+    a * b + c,
+  );
+}
+
+// tests pass
