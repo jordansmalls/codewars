20ba7dcdef5f7ba626a9f254a96388be4b94f0e3
Jordan <smalls.jordan@gmail.com>
2024-07-04 21:48:22 -0400

    receives an input of ints, returns a boolean value determined by if the input int is even or odd

---
+++ is-it-even.java
@@ -0,0 +1,29 @@
+// Is It Even
+/*
+In this Kata we are passing a number (n) into a function.
+Your code will determine if the number passed is even (or not).
+The function needs to return either a true or false.
+Numbers may be positive or negative, integers or floats.
+Floats with decimal part non equal to zero are considered UNeven for this kata.
+*/
+
+public class Kata {
+    public static boolean isEven(double n) {
+        // Check if the number is a float with a decimal part non equal to zero
+        if (n % 1 != 0) {
+            return false;
+        }
+
+        // Check if the integer part of the number is even
+        return ((int) n) % 2 == 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isEven(4));    // true
+        System.out.println(isEven(3));    // false
+        System.out.println(isEven(2.0));  // true
+        System.out.println(isEven(2.5));  // false
+        System.out.println(isEven(-2));   // true
+        System.out.println(isEven(-3.5)); // false
+    }
+}
