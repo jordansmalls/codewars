bcf4d9c210568f7fb0a5a42759fe64b13d0ca60d
Jordan <smalls.jordan@gmail.com>
2024-07-08 23:35:54 -0400

    is it even coding challenge - testing WSL env

--- is-it-even.js
+++ is-it-even.js
@@ -10,14 +10,21 @@
 
 // Floats with decimal part non equal to zero are considered UNeven for this kata.
 
-const testEven = n => {
-    if(n % 2 === 0){
-      return true
-    }else{
-      return false
-    }
-  } 
+// const testEven = n => {
+//     if(n % 2 === 0){
+//       return true
+//     }else{
+//       return false
+//     }
+//   } 
   
+var testEven = n => n % 2 == 0 ? true : false;
+
 // refactored shorthand version
 
 const testEvenAgain = n => !(n%2)
+
+
+console.log(testEven(2)) // true
+console.log(testEven(3)) // false
+console.log(testEven(24)) // true
