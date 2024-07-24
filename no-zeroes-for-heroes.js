f3909f221aa2bb401275cba223fb57e895928950
Jordan <138732777+jordansmalls@users.noreply.github.com>
2024-07-23 20:47:31 -0400

    update ~ added a more practical solution that includes string manipulation

--- no-zeroes-for-heroes.js
+++ no-zeroes-for-heroes.js
@@ -15,7 +15,19 @@ var noBoringZeros = n => {
     return n;
 }
 
+// more practical solution
+const noBoringZerosAgain = n => {
+  return n === 0 ? 0 : Number(n.toString().replace(/0+$/, ''));
+}
+
 console.log(noBoringZeros(1450) === 145 ) // true
 console.log(noBoringZeros(9600000) === 96) // true
 console.log(noBoringZeros(1050) === 105) // true
 console.log(noBoringZeros(-1050) === -105) // true
+
+
+
+console.log(noBoringZerosAgain(1450) === 145 ) // true
+console.log(noBoringZerosAgain(9600000) === 96) // true
+console.log(noBoringZerosAgain(1050) === 105) // true
+console.log(noBoringZerosAgain(-1050) === -105) // true
