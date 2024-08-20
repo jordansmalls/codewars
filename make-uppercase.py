ab5bfe126f35a879a1cbd96fb4c9bb6c8d204bcb
Jordan Smalls <smalls.jordan@gmail.com>
2024-08-20 18:17:59 -0400

    make uppercase ~ returns input string converted to uppercase

---
+++ make-uppercase.py
@@ -0,0 +1,14 @@
+# Make Uppercase
+## Write a function which converts the input string to uppercase.
+
+
+# params {string} s
+# return {string}
+
+## input --> string: containing letters, punctuation, etc
+## return --> string: input string with all chars converted to uppercase
+
+# use static upper method
+
+def make_upper_case(s):
+    return s.upper()
