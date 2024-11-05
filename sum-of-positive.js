641ec6963def4ce774bf30066ff030d8e173b0e3
Jordan Smalls <smalls.jordan@gmail.com>
2024-11-05 12:30:23 -0500

    refactor ~ made func cleaner

--- sum-of-positive.js
+++ sum-of-positive.js
@@ -6,15 +6,25 @@
 
 // Note: if there is nothing to sum, the sum is default to 0.
 
-const positiveSum = arr => {
-    if(arr.length === 1){
-        return 0
-    };
-    let sum = 0;
-    for(let i = 0; i < arr.length; i++){
-        if(arr[i] >= 0){
-            sum += arr[i]
-        }
-    }
-    return sum
-};
+// const positiveSum = arr => {
+//     if(arr.length === 1){
+//         return 0
+//     };
+//     let sum = 0;
+//     for(let i = 0; i < arr.length; i++){
+//         if(arr[i] >= 0){
+//             sum += arr[i]
+//         }
+//     }
+//     return sum
+// };
+
+
+// CLEANER
+
+const positiveSum = arr => arr.filter(idx => idx > 0).reduce((acc,c)=>acc+c,0)
+
+console.log(positiveSum([1,-4,7,12]) == 20) // true
+console.log(positiveSum([23,7,15,5]) == 50) // true
+
+// ALL TESTS PASS
