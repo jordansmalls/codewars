57cdbba74d398f06fa6abfef71729d8f245ea9fb
Jordan <138732777+jordansmalls@users.noreply.github.com>
2025-01-05 16:24:44 -0500

    add: hasDuplicate func returns a boolean based on if input arrays contain a duplicate value

---
+++ contains-duplicate.js
@@ -0,0 +1,31 @@
+// Contains Duplicate
+// Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+
+// Examples:
+
+// Input: nums = [1, 2, 3, 3]
+// Output: true
+
+// Input: nums = [1, 2, 3, 4]
+// Output: false
+
+
+// Create a set and iterate over the input array adding nums to the set
+// find the result of the comparison of the set's size and the arrays length
+    // if the lengths are the same --> return false (there is no duplicate)
+    // if the lengths are not the same --> return true (there is a duplicate)
+
+
+class Solution {
+    /**
+      * @param {number[]} nums
+      * @returns {boolean}
+    */
+   hasDuplicate(nums) {
+        let set = new Set();
+        for(const num of nums) {
+            set.add(num);
+        };
+        return set.size == nums.length == false ? true : false;
+   };
+};
