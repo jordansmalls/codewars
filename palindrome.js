1b4b412bf6c68c48baf60cb2747ac1933fb94d66
Jordan Smalls <smalls.jordan@gmail.com>
2025-01-07 18:58:58 -0500

    refactor: optimize palindrome function for memory efficiency

--- palindrome.js
+++ palindrome.js
@@ -3,8 +3,24 @@
 Your team tasks you with creating a function that can quickly determine if an input string is a palindrome or not. Create a function that returns a boolean value based on if the given string input is a palindrome. Ignore upper or lower casing.
 */
 
-const palindrome = str => str.toLowerCase() === str.toLowerCase().split('').reverse().join('') ? true : false;
+// const palindrome = str => str.toLowerCase() === str.toLowerCase().split('').reverse().join('') ? true : false;
 
+
+// REFACTOR --> improved space efficiency, avoids unnecessary operations (reversing and joining the string), early termination
+
+const palindrome = function(str) {
+    str = str.toLowerCase();
+    let left = 0;
+    let right = str.length - 1;
+    while(left < right) {
+        if(str[left] !== str[right]) {
+            return false;
+        }
+        left++;
+        right--;
+    };
+    return true;
+};
 
 console.log(palindrome('racecar'), true)
 console.log(palindrome('RaCeCaR'), true)
