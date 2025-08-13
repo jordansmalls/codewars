028e390bf6e8b00b76028ce9025a1fd7d80eb958
Jordan Smalls <smalls.jordan@gmail.com>
2025-08-13 16:26:43 -0400

    add: implemented a solution splitting a string into an array of length 5 and returning the len of the arr

---
+++ unusual-five.js
@@ -0,0 +1,18 @@
+// Unusual Five
+/*
+Write a function that always returns 5
+Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+Good luck :)
+*/
+
+// input -> empty
+// output -> 5
+
+/**
+ *
+ * @returns {number}
+ */
+
+const unusualFive = () => "Interstellar is the best movieever".split(" ").length
+
+console.log(unusualFive() == 5) // true
